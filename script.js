d9ac3c9db06cd5d070d1e600665415ae3b06fbe2
Jagholin <pavel.belskiy2@gmail.com>
2022-11-30 16:40:20 +0100

    add javascriptt framework

---
+++ script.js
@@ -0,0 +1,34 @@
+const taskInput = document.querySelector("#task-input");
+const btn_addTask = document.querySelector("#add-task");
+const btn_editTask = document.querySelector("#edit-task");
+const btn_deleteTask = document.querySelector("#delete-task");
+
+// currently chosen element from the todo list
+let chosenTodo = null;
+
+const startingElements = document.querySelectorAll("#todo-list li");
+
+for (const elem of startingElements) {
+    elem.addEventListener("click", (e) => {
+        chosenTodo?.classList.remove("selected");
+        chosenTodo = e.target;
+        chosenTodo.classList.add("selected");
+        console.log(chosenTodo)
+    })
+}
+
+function addTask() {
+
+}
+
+function editTask() {
+
+}
+
+function deleteTask() {
+
+}
+
+btn_addTask.addEventListener("click", addTask);
+btn_deleteTask.addEventListener("click", deleteTask);
+btn_editTask.addEventListener("click", editTask);
